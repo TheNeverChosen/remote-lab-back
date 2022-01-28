e7fb57fce7df8f36cbee88bb2f8499f65541769d
Fernando Ventilari <fernandoventilari@gmail.com>
2022-01-28 14:46:24 -0400

    ws onMessage fixed

--- routes/controllers/ws.js
+++ routes/controllers/ws.js
@@ -1,7 +1,7 @@
 const wsArduinoSrv = require('../../services/wsArduino');
 
 function arduinoPlc(ws, req){
-  ws.on('message', wsArduinoSrv.wsMessage);
+  ws.on('message', wsArduinoSrv.wsReceiveMessage);
   ws.on('close', wsArduinoSrv.wsClose);
   ws.on('error', (err)=>{console.log(err)});
 }
