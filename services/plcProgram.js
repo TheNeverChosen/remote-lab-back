5729d3ad53578a563f57fbd254668fd82539d5d2
Fernando Ventilari <fernandoventilari@gmail.com>
2022-02-03 23:45:13 -0400

    offline PLC code fixed

--- services/plcProgram.js
+++ services/plcProgram.js
@@ -7,7 +7,7 @@ async function launchToArduino(plcFilter, clientProtocol){
   const plc = plcFilter ? await Plc.findOne(plcFilter) : null;
   if(!plc) throw createError(404, 'PLC not found');
 
-  if(!wsArduinoSrv.isPlcOnline(plc.reference)) throw createError('Error: PLC is offline.');
+  if(!wsArduinoSrv.isPlcOnline(plc.reference)) throw createError(400, 'Error: PLC is offline.');
 
   const arduinoMsg = arduinoTranslate.clientToArduinoEmbed(plc.toObject(), clientProtocol, wsArduinoSrv.msgCodes.EMBED);
   wsArduinoSrv.wsSendMessageToPlc(plc.reference, arduinoMsg);
